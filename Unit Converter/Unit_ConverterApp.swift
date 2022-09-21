b946f8348db6ad034b2d98cdaa5f00e2d3d52fef
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2022-09-21 08:44:07 -0700

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Jason Koehn on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
