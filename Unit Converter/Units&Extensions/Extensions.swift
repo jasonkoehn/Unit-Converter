6a9e31c893e1f575edd26293034f33b7d70e1d5e
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2022-12-12 13:04:31 -0700

    New

--- Unit Converter/Units&Extensions/Extensions.swift
+++ Unit Converter/Units&Extensions/Extensions.swift
@@ -22,26 +22,3 @@ func FormatNum(from: NSNumber) -> String {
     formatter.maximumFractionDigits = 4
     return formatter.string(from: from)!
 }
-
-func getColorsFromPicker(pickerColor: Color) -> (red: CGFloat, green: CGFloat, blue: CGFloat) {
-    let colorString = "\(pickerColor)"
-    let colorArray: [String] = colorString.components(separatedBy: " ")
-    var red = 0.0
-    var green = 0.0
-    var blue = 0.0
-    if colorArray.count > 1 {
-        let r: CGFloat = CGFloat((Float(colorArray[1]) ?? 1))
-        let g: CGFloat = CGFloat((Float(colorArray[2]) ?? 1))
-        let b: CGFloat = CGFloat((Float(colorArray[3]) ?? 1))
-//        if (r < 0.0) {r = 0.0}
-//        if (g < 0.0) {g = 0.0}
-//        if (b < 0.0) {b = 0.0}
-//        if (r > 1.0) {r = 1.0}
-//        if (g > 1.0) {g = 1.0}
-//        if (b > 1.0) {b = 1.0}
-        red = r
-        green = g
-        blue = b
-    }
-    return (red, green, blue)
-}
